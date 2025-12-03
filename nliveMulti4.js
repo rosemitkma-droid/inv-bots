bde72bdc79102f4408ed9d96ea51f8f88dc4c554
rosemitkma-droid <rosemitkma@gmail.com>
2025-12-03 03:51:29 +0100

    Update nliveMulti4.js

--- nliveMulti4.js
+++ nliveMulti4.js
@@ -983,7 +983,7 @@ class EnsembleDecisionMaker {
     optimizeThreshold() {
         if (this.recentDecisions.length < 50) return;
 
-        const thresholds = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8];
+        const thresholds = [0.7, 0.75, 0.8]; //[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8];
         let bestThreshold = 0.7;
         let bestScore = -Infinity;
 
