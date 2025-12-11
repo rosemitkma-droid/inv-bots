dcc456fa4db34b7edc340a20b88185d4f0be9b9c
rosemitkma-droid <rosemitkma@gmail.com>
2025-12-12 00:52:14 +0100

    Update kMultiBot.js

--- kMultiBot.js
+++ kMultiBot.js
@@ -10,8 +10,8 @@ class EnhancedDigitDifferTradingBot {
         this.wsReady = false;
 
         this.assets = config.assets || [
-            'R_10', 'R_25', 'R_50', 'R_75', 'R_100'
-            // 'R_100',
+            // 'R_10', 'R_25', 'R_50', 'R_75', 'R_100'
+            'R_100',
         ];
 
         this.config = {
