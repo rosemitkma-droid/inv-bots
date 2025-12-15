6f06876b3612fd41fa7e93c8fd62a5e57368b942
rosemitkma-droid <rosemitkma@gmail.com>
2025-12-15 07:12:11 +0100

    Update kMultiBot.js

--- kMultiBot.js
+++ kMultiBot.js
@@ -1190,7 +1190,7 @@ const bot = new EnhancedDigitDifferTradingBot('hsj0tA0XJoIzJG5', {
     multiplier: 21,
     maxConsecutiveLosses: 3,
     stopLoss: 400,
-    takeProfit: 5000,
+    takeProfit: 5,
     growthRate: 0.05,
     accuTakeProfit: 0.5,
     requiredHistoryLength: 1000,
