1146d37929c8102cc652f61770e9e547af3d3ca3
rosemitkma-droid <rosemitkma@gmail.com>
2026-01-19 09:04:20 +0100

    Update cluadeWill2.js

--- cluadeWill2.js
+++ cluadeWill2.js
@@ -605,7 +605,7 @@ const CONFIG = {
     // Reversal Settings
     REVERSAL_STAKE_MULTIPLIER: 2,
     MAX_REVERSAL_LEVEL: 7,
-    AUTO_CLOSE_ON_RECOVERY: true,
+    AUTO_CLOSE_ON_RECOVERY: false,
 
     // Timeframe Settings (DEFAULT 5 MINUTES)
     TIMEFRAME: SELECTED_TIMEFRAME,
