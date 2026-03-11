623663b078c7d1ca7fe3492f1a60c9695b53ea0d
rosemitkma-droid <rosemitkma@gmail.com>
2026-03-11 18:26:49 +0100

    Update stepIndex2.js

--- stepIndex2.js
+++ stepIndex2.js
@@ -21,7 +21,7 @@ const DEFAULT_CONFIG = {
   apiToken: 'hsj0tA0XJoIzJG5',
   appId:    '1089',
 
-  symbol:        'stpRNG5',
+  symbol:        'stpRNG',
   tickDuration:  3,
   initialStake:  0.35,
   investmentAmount: 100,
@@ -47,7 +47,7 @@ const DEFAULT_CONFIG = {
 // FILE PATHS
 // ══════════════════════════════════════════════════════════════════════════════
 
-const STATE_FILE          = path.join(__dirname, 'STn-grid-state00000005.json');
+const STATE_FILE          = path.join(__dirname, 'STn-grid-state00000006.json');
 const STATE_SAVE_INTERVAL = 5000;
 
 // ══════════════════════════════════════════════════════════════════════════════
