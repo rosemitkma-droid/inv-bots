69c2838dab4692f377a79509ddfbac695f03e84a
rosemitkma-droid <rosemitkma@gmail.com>
2026-05-01 13:56:36 +0100

    Update candlePatternRFm.js

--- candlePatternRFm.js
+++ candlePatternRFm.js
@@ -48,8 +48,8 @@ const CONFIG = {
 const ACTIVE_ASSETS = [
   // 'R_10', 'R_25', 'R_50', 'R_75', 'R_100',
   // '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V',
-  'stpRNG', 'stpRNG2', 'stpRNG3', 'stpRNG4', 'stpRNG5'
-  // 'stpRNG'
+  // 'stpRNG', 'stpRNG2', 'stpRNG3', 'stpRNG4', 'stpRNG5'
+  'R_25'
 ];
 
 // ══════════════════════════════════════════════════════════════════════════════
