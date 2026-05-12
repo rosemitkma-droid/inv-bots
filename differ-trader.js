99b4bdfb2a1c7d72f36e7f0dde335fc2f5838035
rosemitkma-droid <rosemitkma@gmail.com>
2026-05-12 22:16:46 +0100

    Update differ-trader.js

--- differ-trader.js
+++ differ-trader.js
@@ -47,8 +47,8 @@ const BOT_CONFIG = {
         longWindowThreshold: 10,     // >= 10%
         longWindowThresholdHigh: 11, //12%
         // Repeat probability threshold
-        maxRepeatProbability: 19,    // <= 10%
-        minRepeatProbability: 16,    // >= 10%
+        maxRepeatProbability: 20,    // <= 10%
+        minRepeatProbability: 17,    // >= 10%
 
         // Minimum sample size for repeat probability calculation
         minRepeatSample: 10,
@@ -69,7 +69,7 @@ const BOT_CONFIG = {
 // ─────────────────────────────────────────────────────────────────────────────
 // STATE PERSISTENCE
 // ─────────────────────────────────────────────────────────────────────────────
-const STATE_FILE = path.join(__dirname, 'deriv_frequency_bot_02_state.json');
+const STATE_FILE = path.join(__dirname, 'deriv_frequency_bot_03_state.json');
 const STATE_SAVE_INTERVAL = 5000;
 
 class StatePersistence {
