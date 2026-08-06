6b6da7200365250b1e14790f68c61eb4a52c1c79
rosemitkma-droid <rosemitkma@gmail.com>
2026-08-06 11:57:43 +0100

    Update bizCandleRiseFall.js

--- bizCandleRiseFall.js
+++ bizCandleRiseFall.js
@@ -79,8 +79,8 @@ class RestClient {
 // ============================================================
 // FILE PATHS  [RETAINED]
 // ============================================================
-const STATE_FILE = path.join(__dirname, 'bizCandle_016-state.json');
-const HISTORY_FILE = path.join(__dirname, 'bizCandle_016-history.json');
+const STATE_FILE = path.join(__dirname, 'bizCandle_017-state.json');
+const HISTORY_FILE = path.join(__dirname, 'bizCandle_017-history.json');
 const STATE_SAVE_INTERVAL = 5000;  // ms
 
 // ============================================================
@@ -118,7 +118,7 @@ const CONFIG = {
     MAX_MARTINGALE_LEVEL: 1,
     AFTER_MAX_LOSS: 'continue',
     CONTINUE_EXTRA_LEVELS: 4,
-    EXTRA_LEVEL_MULTIPLIERS: [2.0, 2.1, 2.2, 2.3], //[2.0, 2.1, 2.1, 2.2, 2.2, 2.2, 2.3]
+    EXTRA_LEVEL_MULTIPLIERS: [2.2, 2.2, 2.2, 2.3], //[2.0, 2.1, 2.1, 2.2, 2.2, 2.2, 2.3]
     AUTO_COMPOUNDING: false,
     COMPOUND_PERCENTAGE: 0.24,
     STOP_LOSS: 152,
